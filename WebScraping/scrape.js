b930065eeaf8bee3bc509ebe28592168c6c7eaa5
Alex Skreen <skreenalex@gmail.com>
2020-05-08 13:14:51 -0700

    get workout details from comptrain

--- WebScraping/scrape.js
+++ WebScraping/scrape.js
@@ -7,26 +7,19 @@ request("https://comptrain.co/wod/", (error, response, html) => {
     const $ = cheerio.load(html);
 
     const wodDate = $(".wod-date");
+    const wodInfo = $(".wod-info");
     // console.log(wodDate.html);
     // console.log(wodDate.text);
     const output = wodDate.find('h5').text();
-    console.log(output);
+    const output2 = wodInfo.find('h3').text();
+    const output3 = wodInfo.find('p').text();
+    // console.log(output);
+    // console.log(output2);
+    console.log(output3);
     // const output = wodDate.children("h5").text();
     // console.log(output);
   }
+    else {
+    console.log("nope");
+  }
 });
-
-// request("https://www.crossfit.com/", (error, response, html) => {
-//   if (!error && response.statusCode == 200) {
-//     console.log(html);
-//   }
-// });
-
-// request("https://www.crossfitlinchpin.com/", (error, response, html) => {
-//   if (!error && response.statusCode == 200) {
-//     console.log(html);
-//   }
-//   else {
-//     console.log("nope");
-//   }
-// });
