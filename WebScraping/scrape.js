a1eca99c6d926f392b9f472011ca458a34d8ab6b
Alex Skreen <skreenalex@gmail.com>
2020-05-08 13:32:22 -0700

    scrape for all elements within a class, or scrape for certain attributes

--- WebScraping/scrape.js
+++ WebScraping/scrape.js
@@ -10,14 +10,25 @@ request("https://comptrain.co/wod/", (error, response, html) => {
     const wodInfo = $(".wod-info");
     // console.log(wodDate.html);
     // console.log(wodDate.text);
-    const output = wodDate.find('h5').text();
-    const output2 = wodInfo.find('h3').text();
-    const output3 = wodInfo.find('p').text();
+    // const output = wodDate.find('h5').text();
+    // const output2 = wodInfo.find('h3').text();
+    // const output3 = wodInfo.find('p').text();
     // console.log(output);
     // console.log(output2);
-    console.log(output3);
-    // const output = wodDate.children("h5").text();
-    // console.log(output);
+    // console.log(output3);
+    // const output4 = wodInfo
+    //   .find('p')
+    //   .next()
+    //   .text();
+    // console.log(output4);
+
+    $('.wod-info p').each((i, el) => {
+      const item = $(el).text();
+      // const link = $(el).attr('href');
+
+      console.log(item);
+      // console.log(link);
+    });
   }
     else {
     console.log("nope");
