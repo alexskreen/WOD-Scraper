4d2fa541f5a61e83f89252db31639a311034f10a
Alex Skreen <skreenalex@gmail.com>
2020-05-08 17:40:14 -0700

    export potusParse and update potusScraper

--- WebScraping/potusScraper.js
+++ WebScraping/potusScraper.js
@@ -1,5 +1,6 @@
 const rp = require("request-promise");
 const $ = require("cheerio");
+const potusParse = require("./potusParse");
 const url =
   "https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States";
 
@@ -8,10 +9,18 @@ rp(url)
     //success!
     const wikiUrls = [];
     for (let i = 0; i < 45; i++) {
-      wikiUrls.push($("b > a", html)[i].attribs.href);
+      wikiUrls.push($("big > a", html)[i].attribs.href);
     }
-    console.log(wikiUrls);
+    return Promise.all(
+      wikiUrls.map(function (url) {
+        return potusParse("https://en.wikipedia.org" + url);
+      })
+    );
+  })
+  .then(function (presidents) {
+    console.log(presidents);
   })
   .catch(function (err) {
     //handle error
+    console.log(err);
   });
