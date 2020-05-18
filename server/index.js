0715504926cda130853b3d5d64f7dee6d06cc0ea
Alex Skreen <skreenalex@gmail.com>
2020-05-18 11:23:47 -0700

    fix typo in index.js. everything on the example should work now. going to have to re-tool vars once I get the example DB running

--- server/index.js
+++ server/index.js
@@ -2,40 +2,32 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-app.use(bodyParser.json())
-app.use(function(req, res, next) {
+const scrapers = require("./scrapers");
+const db = require("./db");
+
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // disabled for security on local
-  res.header("Access-Control-Allow-Origin", "Content-type");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
 app.get("/creators", async (req, res) => {
-  const creators = [
-    { name: "Code Drip", img: "https://" },
-    { name: "Dave Lee", img: "https://" },
-    { name: "MKBHD", img: "https://" },
-  ]
-  // todo: GET from db
-  res.send(creators)
-})
+  const creators = await db.getAllCreators();
+  res.send(creators);
+});
 
 app.post("/creators", async (req, res) => {
-  console.log(req.body)
-  // todo: Scrape channel
-  // todo: Add to DB
-  res.send('success')
-})
+  console.log(req.body);
+  const channelData = await scrapers.scrapeChannel(req.body.channelURL);
+  const creators = await db.insertCreator(
+    channelData.name,
+    channelData.avatarURL,
+    req.body.channelURL
+  );
+  res.send(creators);
+});
 
-
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
-
-
-
-// this is all probably wrong. giving it a shot. 
-// await page.$x("<xPath>");
-// const elements = await page.$x("<xPath>");
-// await elements[0].click(); 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
