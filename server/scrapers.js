962a3b853b55fd7bad4a0435e0aeb217244fcd42
Alex Skreen <skreenalex@gmail.com>
2020-05-18 16:34:19 -0700

    update scrapers. the scraper is working now. I just need to tweak the form and what I want to get out of the scraper

--- server/scrapers.js
+++ server/scrapers.js
@@ -1,15 +1,16 @@
 const puppeteer = require("puppeteer");
 
 async function scrapeChannel(url) {
+
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
 
-  const [el] = await page.$x(
-    "/html/body/ytd-app/div/ytd-page-manager/ytd-browse/div[3]/ytd-c4-tabbed-header-renderer/app-header-layout/div/app-header/div[2]/div[2]/div/div[1]/div/div[1]/ytd-channel-name/div/div/yt-formatted-string"
-  );
-  const text = await el.getProperty("textContent");
+  const [el] = await page.$x('/html/body/ytd-app/div/ytd-page-manager/ytd-browse/div[3]/ytd-c4-tabbed-header-renderer/app-header-layout/div/app-header/div[2]/div[2]/div/div[1]/div/div[1]/ytd-channel-name/div/div/yt-formatted-string');
+  const text = await el.getProperty('textContent');
   const name = await text.jsonValue();
+
+  //I think I will be getting most of mmy information like I did on 9-11
 
   const [el2] = await page.$x('//*[@id="img"]');
   const src = await el2.getProperty('src');
@@ -21,7 +22,7 @@ async function scrapeChannel(url) {
   return { name, avatarURL };
 }
 
-scrapeChannel('https://www.youtube.com/c/codedrip');
+scrapeChannel("https://www.youtube.com/channel/UCRLEADhMcb8WUdnQ5_Alk7g");
 
 // module.exports = {
 //   scrapeChannel,
