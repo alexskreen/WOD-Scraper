ce15f53b9091338c27c52526fc168cb57bf3e1b2
Alex Skreen <skreenalex@gmail.com>
2020-05-18 17:08:19 -0700

    test scraper with comptrain. successfully pulling out the data that I need. I am trying to figure out how to get each P tag without doing individual calls

--- server/scrapers.js
+++ server/scrapers.js
@@ -6,23 +6,31 @@ async function scrapeChannel(url) {
   const page = await browser.newPage();
   await page.goto(url);
 
-  const [el] = await page.$x('/html/body/ytd-app/div/ytd-page-manager/ytd-browse/div[3]/ytd-c4-tabbed-header-renderer/app-header-layout/div/app-header/div[2]/div[2]/div/div[1]/div/div[1]/ytd-channel-name/div/div/yt-formatted-string');
+  const [el] = await page.$x(
+    '//*[@id="page"]/section[2]/div[2]/div[1]/div/div/div/h5'
+  );
   const text = await el.getProperty('textContent');
   const name = await text.jsonValue();
 
+  const [el2] = await page.$x(
+    '//*[@id="page"]/section[2]/div[2]/div[2]/div/div[1]/h3'
+  );
+  const text2 = await el2.getProperty("textContent");
+  const wod = await text2.jsonValue();
+
   //I think I will be getting most of mmy information like I did on 9-11
 
-  const [el2] = await page.$x('//*[@id="img"]');
-  const src = await el2.getProperty('src');
-  const avatarURL = await src.jsonValue();
+  // const [el2] = await page.$x('//*[@id="img"]');
+  // const src = await el2.getProperty('src');
+  // const avatarURL = await src.jsonValue();
 
   browser.close();
 
-  console.log({ name, avatarURL });
-  return { name, avatarURL };
+  console.log({ name, wod });
+  return { name, wod };
 }
 
-scrapeChannel("https://www.youtube.com/channel/UCRLEADhMcb8WUdnQ5_Alk7g");
+scrapeChannel("https://comptrain.co/wod/");
 
 // module.exports = {
 //   scrapeChannel,
