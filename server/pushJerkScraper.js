f8bbc3b4d667a89b943ee0311b3e139f87cb3599
Alex Skreen <skreenalex@gmail.com>
2020-05-19 16:17:21 -0700

    create push jerk scraper

---
+++ server/pushJerkScraper.js
@@ -0,0 +1,21 @@
+const puppeteer = require("puppeteer");
+
+async function scrapeSite(url) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  const [el] = await page.$x('//*[@id="post-4389"]/div/header/h2/a');
+  const txt = await el.getProperty("textContent");
+  const date = await txt.jsonValue();
+
+  const [el2] = await page.$x('//*[@id="post-4389"]/div/div');
+  const txt2 = await el2.getProperty("textContent");
+  const wod = await txt2.jsonValue();
+
+  console.log({ date, wod });
+
+  browser.close();
+}
+
+scrapeSite("http://pushjerk.com/");
