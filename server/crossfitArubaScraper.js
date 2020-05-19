4b443664e5cb988792ffc9ae8edad7b2d7b7c2f8
Alex Skreen <skreenalex@gmail.com>
2020-05-19 12:01:09 -0700

    create CrossFit Aruba scraper

---
+++ server/crossfitArubaScraper.js
@@ -0,0 +1,21 @@
+const puppeteer = require("puppeteer");
+
+async function scrapeSite(url) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  const [el] = await page.$x('//*[@id="entry-10889"]/div/div[1]/h3/a');
+  const txt = await el.getProperty("textContent");
+  const date = await txt.jsonValue();
+
+  const [el2] = await page.$x('//*[@id="entry-10889"]/div/div[1]/div[2]/p');
+  const txt2 = await el2.getProperty("textContent");
+  const wod = await txt2.jsonValue();
+
+  console.log({ date, wod });
+
+  browser.close();
+}
+
+scrapeSite("http://www.musclesandlungscrossfit.com/");
