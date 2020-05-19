e767cfe25c0f035241dee6cc9aff459be214a32c
Alex Skreen <skreenalex@gmail.com>
2020-05-19 11:12:28 -0700

    create upheaval scraper

---
+++ server/upheavalScraper.js
@@ -0,0 +1,23 @@
+const puppeteer = require("puppeteer");
+
+async function scrapeSite(url) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  const [el] = await page.$x(
+    '//*[@id="post-4741"]/div/header/div/span[1]/a/time'
+  );
+  const txt = await el.getProperty("textContent");
+  const date = await txt.jsonValue();
+
+  const [el2] = await page.$x('//*[@id="post-4741"]/div/div/figure/figcaption');
+  const txt2 = await el2.getProperty("textContent");
+  const wod = await txt2.jsonValue();
+
+  console.log({ date, wod });
+
+  browser.close();
+}
+
+scrapeSite("https://crossfitupheaval.com/news/");
