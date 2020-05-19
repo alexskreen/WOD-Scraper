f1061ee485ebf40bd939f0075121dec5b65daafc
Alex Skreen <skreenalex@gmail.com>
2020-05-19 10:43:24 -0700

    invictus was a bit odd becuase of the 'read more' on every day's workout. Used recorder

---
+++ server/invictusScraper.js
@@ -0,0 +1,33 @@
+const puppeteer = require("puppeteer");
+async function scrapeSite(url) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  const navigationPromise = page.waitForNavigation();
+
+  await page.goto("https://www.crossfitinvictus.com/wod/");
+
+  await page.setViewport({ width: 754, height: 865 });
+
+  await page.waitForSelector(
+    ".main > .post-88438 > .entry-content > p > .read-more"
+  );
+  await page.click(".main > .post-88438 > .entry-content > p > .read-more");
+
+  await navigationPromise;
+
+  const [el] = await page.$x("/html/body/div[1]/div[2]/main/article/header/div/h2/a");
+  const txt = await el.getProperty("textContent");
+  const date = await txt.jsonValue();
+
+  const [el2] = await page.$x("/html/body/div[1]/div[2]/main/article/div[1]");
+  const txt2 = await el2.getProperty("textContent");
+  const wod = await txt2.jsonValue();
+
+
+  console.log({ date, wod });
+
+  await browser.close();
+};
+
+scrapeSite("https://www.crossfitinvictus.com/wod/");
