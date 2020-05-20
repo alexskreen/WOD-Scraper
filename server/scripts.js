ec9dbb3fe83593557b3f5f6ccb5985ebe99c7f07
Alex Skreen <skreenalex@gmail.com>
2020-05-20 13:48:23 -0700

    add SO ideas on how to fix select issue

--- server/scripts.js
+++ server/scripts.js
@@ -1,6 +1,23 @@
 // document.getElementById("gym-select").addEventListener("change", function () {
 //   console.log("You selected: ", this.value);
 // });
+// $(document).ready(function(){
+//     $('.gym-select').click(function(){
+//         var userGym = $(".gym-select").val();
+//         //   Note the url below. It adds catid=(#categoryBox value from above).
+//         $.ajax({
+//             type:'GET',
+//             url:'/test?catid=' + catID,
+//             dataType:'json',
+//             success: function(data){
+//                 $('#games').empty();
+//                 for (var i = 0; i < data.length; i++){
+//                     $('#games').append('<option>'+data[i].gamename+'</option>')
+//                 }
+//             }
+//         });
+//     });
+// });
 
 var userSelect = document.getElementById("gym-select");
 var userGym = userSelect.options[userSelect.selectedIndex].value;
