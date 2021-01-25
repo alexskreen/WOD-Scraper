a594f00ccbeafc77ed8ab67428fdc4644dc4e6ea
Alex Skreen <skreenalex@gmail.com>
2021-01-25 15:23:23 -0800

    fixed mainsite scraper

--- server/mainsiteScraper.js
+++ server/mainsiteScraper.js
@@ -6,23 +6,26 @@ async function scrapeSite(url) {
   await page.goto(url);
 
   const [el] = await page.$x(
-    '//*[@id="dailies-column-0-0"]/section/div[1]/div/h2/a/span[2]'
+    '//*[@id="archives"]/div[1]/div[1]/div/div/div[2]/h3/a'
   );
   const txt = await el.getProperty("textContent");
   const date = await txt.jsonValue();
 
   const [el2] = await page.$x(
-    '//*[@id="dailies-column-0-0"]/section/div[2]/div[1]/div/div/article/div'
+    '/html/body/div[3]/section[2]/div[1]/div[1]/div/div/div[2]/div/div[1]/p[1]/strong'
   );
   const txt2 = await el2.getProperty("textContent");
   const wod = await txt2.jsonValue();
 
   browser.close();
 
-  return({ date, wod });
+  console.log(date, wod);
 
+  // return({ date, wod });
 }
 
-module.exports = {
-  scrapeSite,
-};
+// module.exports = {
+//   scrapeSite('https://www.crossfit.com/workout/')
+// };
+
+scrapeSite('https://www.crossfit.com/workout/');
